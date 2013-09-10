78fd2775cd0d8d158da138bcd608e93c82a04b22
Marc-Daniel Ortega <globulon@gmail.com>
2013-09-10 20:33:07 +0200

    First commit

---
+++ project/Build.scala
@@ -0,0 +1,47 @@
+import sbt._
+import Keys._
+
+object Properties {
+  lazy val appVer         = "0.1-SNAPSHOT"
+  lazy val scalaVer       = "2.10.2"
+  lazy val scalaTestVer   = "2.0.M6"
+ }
+
+object BuildSettings {
+  import Properties._
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization        := "com.promindis",
+    version             := appVer,
+    scalaVersion        := scalaVer,
+    scalacOptions       := Seq("-unchecked", "-deprecation"),
+    ivyValidate         := false
+
+  )
+}
+
+object Resolvers {
+  lazy val typesafeReleases = "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+  lazy val scalaToolsRepo = "sonatype-oss-public" at "https://oss.sonatype.org/content/groups/public/"
+}
+
+object ApplicationDependencies {
+  import Properties._
+}
+
+object TestDependencies {
+  import Properties._
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVer % "test" withSources()
+}
+
+object ApplicationBuild extends Build {
+  import Resolvers._
+  import TestDependencies._
+  import BuildSettings._
+
+  lazy val fpInScala = Project(
+    "lp-in-scala",
+    file("."),
+    settings = buildSettings ++ Seq(resolvers += typesafeReleases) ++  
+              Seq (libraryDependencies ++= Seq(scalaTest))
+  )
+}
---
+++ src/main/scala/math/lp/Matrix.scala
@@ -0,0 +1,5 @@
+package math.lp
+
+class Matrix {
+
+}
